6275ccc693ecc6df1bbc244d1678df25ff9348e4
Eshgin Guluzade <eshgin.guluzade@playtech.com>
2024-05-01 12:42:34 +0300

    Added files for yt How-to-use-OpenAI-API-To-Create-Real-Apps

---
+++ How-to-use-OpenAI-API-To-Create-Real-Apps/openapi-project.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+import datetime
+
+client = OpenAI(api_key="sk-proj-vts8a1Yv1qQ95y5sOBEkT3BlbkFJunsNhCWHVxKxr8w12Acs")
+
+
+today = datetime.datetime.now().strftime("%B %d")
+
+response = client.chat.completions.create(
+        messages = [{"role":"user", "content":f"What is the most famous thing that happened on {today} in history?"}],
+        model = "gpt-3.5-turbo",
+        )
+
+if response.choices:
+    print(response.choices[0].message.content)
+else:
+    print("No response found.")
